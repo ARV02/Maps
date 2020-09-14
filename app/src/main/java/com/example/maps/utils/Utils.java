f82ba695777b9d90d73b7fd2713f2ea634e827cf
Chaips <ar0439708@gmail.com>
2020-09-14 13:36:15 -0500

    Terminacion del trazado de las rutas

--- app/src/main/java/com/example/maps/utils/Utils.java
+++ app/src/main/java/com/example/maps/utils/Utils.java
@@ -4,10 +4,14 @@ import android.content.Context;
 
 import com.google.android.gms.maps.GoogleMap;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class Utils {
     public static Coordenadas coordenadas = new Coordenadas();
+    public static List<List<HashMap<String, String>>> routes = new ArrayList<>();
     public static void markersDefault(GoogleMap map, Context context){
         new Marcadores(map, context).addMarkersDefault();
     }
