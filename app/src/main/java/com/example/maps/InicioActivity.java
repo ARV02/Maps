2479c87effc0414384386453286409b298d8789a
Chaips <ar0439708@gmail.com>
2020-09-14 22:21:26 -0500

    Finalizacion

--- app/src/main/java/com/example/maps/InicioActivity.java
+++ app/src/main/java/com/example/maps/InicioActivity.java
@@ -12,6 +12,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Camera;
+import android.graphics.Color;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -19,6 +20,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -34,6 +36,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -43,31 +47,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class InicioActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnMapClickListener, GoogleMap.OnMarkerClickListener {
-    Context context;
+public class InicioActivity extends AppCompatActivity implements OnMapReadyCallback {
     GoogleMap map;
-    JsonObjectRequest jsonObjectRequest;
-    RequestQueue requestQueue;
-    Location location;
-    GPS_controler gpsTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        gpsTracker = new GPS_controler(getApplicationContext());
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment);
         mapFragment.getMapAsync(this);
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
-    }
-
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-        Utils.coordenadas.setOrigenLat(latLng.latitude);
-        Utils.coordenadas.setDestinoLat(latLng.longitude);
-        Toast.makeText(InicioActivity.this, "Select the icon", Toast.LENGTH_SHORT).show();
-
     }
 
     @Override
@@ -84,232 +74,39 @@ public class InicioActivity extends AppCompatActivity implements OnMapReadyCallb
             return;
         }
         map.setMyLocationEnabled(true);
-        LatLng we = new LatLng(19.651211, -99.167167);
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(we, 10));
-        Utils.markersDefault(map, getApplicationContext());
-        map.setOnMapClickListener(this);
-        map.setOnMarkerClickListener(this);
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        ArrayList<LatLng>points = null;
+        PolylineOptions lineOptions = null;
+        for(int i = 0; i< Utils.routes.size(); i++){
+            Log.d("aqui", String.valueOf(Utils.routes.size()));
+            points = new ArrayList<LatLng>();
+            lineOptions = new PolylineOptions();
 
-    }
 
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        AlertShow(marker.getTitle(),marker.getPosition());
-        return false;
+            List<HashMap<String, String>> path = Utils.routes.get(i);
+
+
+            for(int j=0;j<path.size();j++){
+                HashMap<String,String> point = path.get(j);
+
+                double lat = Double.parseDouble(point.get("lat"));
+                double lng = Double.parseDouble(point.get("lng"));
+                LatLng position = new LatLng(lat, lng);
+
+                points.add(position);
+            }
+
+
+            lineOptions.addAll(points);
+
+            lineOptions.width(9);
+
+            lineOptions.color(Color.BLUE);
+        }
     }
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
-    }
-
-    @Override
-    public void onMapClick(LatLng latLng) {
-
-    }
-
-    public void AlertShow(String title, final LatLng latLng){
-        AlertDialog.Builder builder= new AlertDialog.Builder(InicioActivity.this);
-
-        builder.setMessage("Desea ir este punto?");
-        builder.setTitle(title);
-        builder.setCancelable(false);
-
-        builder.setPositiveButton("Si", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Utils.coordenadas.setDestinoLat(latLng.latitude);
-                Utils.coordenadas.setDestinoLng(latLng.longitude);
-
-                new MyAsyncTask().execute(0);
-
-            }
-        });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
-
-
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
-    }
-
-    public  class MyAsyncTask extends AsyncTask<Integer, Integer, String> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(Integer... integers) {
-            try {
-                while (location == null) {
-                    location = gpsTracker.getLocation();
-                    publishProgress(1);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            location = gpsTracker.getLocation();
-            publishProgress(2);
-            return "Fin";
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-
-            if (values[0] == 0) {
-                Log.d("Asyntask", "null");
-            } else {
-                Log.d("Asyntask", "Coordenadas");
-                Toast.makeText(InicioActivity.this, "LISTO", Toast.LENGTH_SHORT).show();
-                Utils.coordenadas.setOrigenLat(location.getLatitude());
-                Utils.coordenadas.setOrigenLng(location.getLongitude());
-                Log.d("Asyntask", String.valueOf(location.getLatitude()));
-                Log.d("Asyntask", String.valueOf(location.getLongitude()));
-                ObtenerRuta(String.valueOf(Utils.coordenadas.getOrigenLat()), String.valueOf(Utils.coordenadas.getOrigenLng()),
-                        String.valueOf(Utils.coordenadas.getDestinoLat()), String.valueOf(Utils.coordenadas.getDestinoLng()));
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-            Log.d("asyntask", "FIN");
-        }
-    }
-
-    private Boolean permissionsGranted() {
-        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == ((PackageManager.PERMISSION_GRANTED));
-    }
-    private void startInstalledAppDetailsActivity() {
-        Intent i = new Intent();
-        i.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-        i.addCategory(Intent.CATEGORY_DEFAULT);
-        i.setData(Uri.parse("package:" + getPackageName()));
-        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(final int requestCode, @NonNull final String[] permissions, @NonNull final int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == 123) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-            } else {
-
-                Toast.makeText(this, "You didn't give permission to access device location", Toast.LENGTH_LONG).show();
-                startInstalledAppDetailsActivity();
-            }
-        }
-    }
-    public void ObtenerRuta(String latInicial, String lngInicial, String latFinal, String lngFinal){
-
-        String url = "https://maps.googleapis.com/maps/api/directions/json?origin=" + latInicial + "," + lngInicial + "&destination=" + latFinal + "," + lngFinal + "&key=AIzaSyCd5cH4hiWh7UuN4SO_wS6Ld13ZCy-mN_A&mode=drive";
-
-
-
-        jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                JSONArray jRoutes = null;
-                JSONArray jLegs = null;
-                JSONArray jSteps = null;
-
-
-                try {
-
-                    jRoutes = response.getJSONArray("routes");
-
-
-                    for(int i=0;i<jRoutes.length();i++){
-
-                        jLegs = ( (JSONObject)jRoutes.get(i)).getJSONArray("legs");
-                        List<HashMap<String, String>> path = new ArrayList<HashMap<String, String>>();
-
-                        for(int j=0;j<jLegs.length();j++){
-                            jSteps = ( (JSONObject)jLegs.get(j)).getJSONArray("steps");
-
-
-                            for(int k=0;k<jSteps.length();k++){
-
-                                String polyline = "";
-                                polyline = (String)((JSONObject)((JSONObject)jSteps.get(k)).get("polyline")).get("points");
-                                List<LatLng> list = decodePoly(polyline);
-
-                                for(int l=0;l<list.size();l++){
-
-                                    HashMap<String, String> hm = new HashMap<String, String>();
-                                    hm.put("lat", Double.toString(((LatLng)list.get(l)).latitude) );
-                                    hm.put("lng", Double.toString(((LatLng)list.get(l)).longitude) );
-                                    path.add(hm);
-
-                                }
-                            }
-
-                            Utils.routes.add(path);
-
-                            Intent intent = new Intent(InicioActivity.this, MainActivity.class);
-                            startActivity(intent);
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }catch (Exception e){
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getApplicationContext(), "No se puede conectar "+error.toString(), Toast.LENGTH_LONG).show();
-                System.out.println();
-                Log.d("ERROR: ", error.toString());
-            }
-        }
-        );
-
-        requestQueue.add(jsonObjectRequest);
-
-    }
-
-
-    private List<LatLng> decodePoly(String encoded) {
-
-        List<LatLng> poly = new ArrayList<LatLng>();
-        int index = 0, len = encoded.length();
-        int lat = 0, lng = 0;
-
-        while (index < len) {
-            int b, shift = 0, result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlat = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lat += dlat;
-
-            shift = 0;
-            result = 0;
-            do {
-                b = encoded.charAt(index++) - 63;
-                result |= (b & 0x1f) << shift;
-                shift += 5;
-            } while (b >= 0x20);
-            int dlng = ((result & 1) != 0 ? ~(result >> 1) : (result >> 1));
-            lng += dlng;
-
-            LatLng p = new LatLng((((double) lat / 1E5)),
-                    (((double) lng / 1E5)));
-            poly.add(p);
-        }
-
-        return poly;
     }
 }
